9aca3f97146046b3d33b5f72221857887195c611
Garnetty97 <garnetyeates@gmail.com>
2017-05-14 01:42:45 -0400

    Basic Shit

--- PvpPlugin/src/com/gmail/garnetyeates/pvpplugin/PvpPlugin.java
+++ PvpPlugin/src/com/gmail/garnetyeates/pvpplugin/PvpPlugin.java
@@ -5,16 +5,21 @@ import java.util.Random;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.garnetyeates.pvpplugin.arrowrain.ArrowRainCommandExecutor;
 import com.gmail.garnetyeates.pvpplugin.arrowrain.ArrowRainListener;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class PvpPlugin extends JavaPlugin implements CommandExecutor {
 	
 	private Server server = Bukkit.getServer();
 	
 	public static Random random = new Random();
+	
+	public static String arrowStormName = ChatColor.GOLD + "" + ChatColor.BOLD + "Arrow Storm";
 	
 	@Override
 	public void onEnable() {
@@ -26,6 +31,11 @@ public class PvpPlugin extends JavaPlugin implements CommandExecutor {
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	public static boolean isMyInventoryFull(Player p) {
+		if (p.getInventory().firstEmpty() == -1) return true;
+		else return false;
 	}
 	
 }
